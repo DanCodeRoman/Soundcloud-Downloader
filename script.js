b61f2be834dfb3399cc627428ec4b3966a1b6dfe
DanCodeRoman <0danroman@gmail.com>
2025-03-10 16:43:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Replace with your SoundCloud client ID
-const CLIENT_ID = "2t9loNQH90kzJcsFCODdigxfp325aq4z";
+const CLIENT_ID = "JGeUlG7kXbAKAKq3ykUdXoVIxnDgEuJz";
 
 // Function to resolve a SoundCloud URL (track or playlist)
 async function resolveSoundCloudUrl(url) {
